0ccfb989c5a6880ed214833ee7dafd1245b6aad9
Thiago <thiagomoutinho34@gmail.com>
2022-06-30 14:14:50 -0300

    calculadora javaScript

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+const cal1 = document.getElementById('#cal1')
+const cal2 = document.getElementById('#cal2')
+const result = document.getElementById('#result')
+
+function calcular(cal1, cal2) { 
+    if(result === "+")
+        return cal1 + cal2;
+    } /* if (result === "-") {
+        return cal1 - cal2;
+    } if (result === "*") {
+        return cal1 - cal2;
+    } if(result === "/") {
+        return cal1 / cal2;
+    } */
